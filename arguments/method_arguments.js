d28ca8d25a4fd37b6b66ad0d364ca6ca7b7a5980
Djory Krache <djorykrache@gmail.com>
2018-09-23 13:33:35 +0200

    apply call examples

--- arguments/method_arguments.js
+++ arguments/method_arguments.js
@@ -6,3 +6,19 @@ function sum(a=1, b=1, c=1, ...e) {
 }
 console.log(sum(...[1,2,3,4,5,6,7])); // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 1 + 1
 console.log(sum()); // 1 + 1 + 1
+
+var squall = {name: 'Squall', power:7};
+var minotaur = {name: 'Minotaur'};
+
+var attack = function(target, damage){
+    console.log(`${this.name} use attack : ${damage * this.power} on ${target.name} `);
+
+};
+var defend = function(){
+    console.log(`${this.name} defends`);
+};
+
+attack.call(squall, minotaur, 10);
+defend.call(squall, minotaur);
+attack.apply(squall, [minotaur, 10]);
+defend.apply(minotaur);
